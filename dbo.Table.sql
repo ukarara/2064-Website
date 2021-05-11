babddd1ff4ef18533a012ddeca4850d8b3f551f1
Ukarara <47874847+ukarara@users.noreply.github.com>
2021-05-11 14:56:42 +0800

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Card]
+(
+	[id] INT NOT NULL PRIMARY KEY , 
+    [Cost] INT NOT NULL, 
+    [ATK] INT NOT NULL, 
+    [DEF] INT NOT NULL, 
+    [CardImg] IMAGE NOT NULL, 
+    [OriImg] IMAGE NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL
+)
